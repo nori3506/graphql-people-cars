7f2853feb407b62dcd1c72cca10cc2bd0e57a43a
Nori <loveandfight2186@yahoo.co.jp>
2021-10-28 16:52:18 +0900

    create AddCar.js for car form

---
+++ client/src/components/form/AddCar.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { Form, Input, Button } from "antd";
+import { useMutation } from "@apollo/client";
+import { v4 as uuidv4 } from "uuid";
+import { ADD_PERSON, GET_PEOPLE } from "../../queries";
+
+const Addcar = () => {
+  return <div></div>;
+};
+
+export default Addcar;
