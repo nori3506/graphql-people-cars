377d7a562121db0d7213ccb80b63c50b2ad11bf6
Nori <loveandfight2186@yahoo.co.jp>
2021-10-28 14:45:11 +0900

    yarn add antd graphql lodash uuid apollo-client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editaaa <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import AddContact from "./components/form/AddContact";
+import Title from "./components/layouts/Title";
+import Contacts from "./components/lists/Contacts";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <Title />
+      <AddContact />
+      <Contacts />
+    </div>
+  </ApolloProvider>
+);
 export default App;
